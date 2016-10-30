0d1325db4b352a1d6ee7ac08ca8ecb30bcdfcf36
jsorbo <jsorbo@gmail.com>
2016-10-29 20:07:58 -0400

    save prior to d3-cloud install

--- assets/js/angular/directive.js
+++ assets/js/angular/directive.js
@@ -2,10 +2,34 @@
  * Created by sinukoll on 10/23/2016.
  */
 
-(function(angular) {
+(function (angular) {
     'use strict';
-    function MainDirective(){};
+
+    function wordCloud($window) {
+        return {
+            restrict: "EA",
+            scope: false,
+            template: "<svg width='850' height='200'></svg>",
+            link: function (scope, elem, attrs) {
+                var wordData = [];
+                var padding = 20;
+                var xScale, yScale, xAxisGen, yAxisGen, lineFun;
+                var d3 = $window.d3;
+                var rawSvg = elem.find('svg');
+                var svg = d3.select(rawSvg[0]);
+
+                function setParameters() {
+
+
+                }
+
+                function draw() {
+
+                }
+            }
+        };
+    };
     angular
         .module('cs5331')
-        .directive('mainDirective',[MainDirective])
-    })(window.angular);
+        .directive('wordCloud', [wordCloud, $window])
+})(window.angular);
